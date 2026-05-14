94c6f575ae92b7f70605e18050bbf514885cd0cc
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2026-05-14 11:53:46 +0000

    autocmds : add filetype for *.tmpl

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -15,3 +15,29 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern  = '*',
   callback = function() vim.highlight.on_yank() end,
 })
+
+-- Set filetype for template files based on their content or extension
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = '*.zsh.tmpl',
+  callback = function() vim.bo.filetype = 'zsh' end,
+})
+
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = '*.toml.tmpl',
+  callback = function() vim.bo.filetype = 'toml' end,
+})
+
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = '*.yaml.tmpl',
+  callback = function() vim.bo.filetype = 'yaml' end,
+})
+
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = '*.yaml.tmpl',
+  callback = function() vim.bo.filetype = 'yaml' end,
+})
+
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = '*.json.tmpl',
+  callback = function() vim.bo.filetype = 'json' end,
+})
