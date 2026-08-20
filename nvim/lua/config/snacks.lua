a7c403572ebec23a8ba6a5ab498d870765ab0b6e
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2026-08-20 08:52:32 +0000

    fix: remove section startup as we use lze

--- nvim/lua/config/snacks.lua
+++ nvim/lua/config/snacks.lua
@@ -24,7 +24,6 @@ require('snacks').setup({
     sections = {
       { section = "header" },
       { section = "keys", gap = 1, padding = 1 },
-      { section = "startup" },
     },
   },
   indent   = {
